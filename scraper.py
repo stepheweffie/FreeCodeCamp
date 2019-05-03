8a1dab9d9d660f96540cb4f9b7b742e7b8c8222c
wtata <wtata@protonmail.com>
2019-05-03 17:43:20 -0400

    Start scraper for freecodecamp.org modules

---
+++ scraper.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = webdriver.ChromeOptions()
+options.binary_location = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('https://learn.freecodecamp.org/')
+wait = WebDriverWait(driver, 15)
+
+parent = driver.find_elements_by_tag_name('ul')
+for child in parent:
+    print(child)
+
+

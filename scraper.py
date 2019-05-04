8d957180346d145979603a9b11a54372fcd320d8
wtata <wtata@protonmail.com>
2019-05-03 20:24:32 -0400

    Clean up unusable lines

--- scraper.py
+++ scraper.py
@@ -12,25 +12,7 @@ driver = webdriver.Chrome(chrome_options=options)
 driver.get('https://learn.freecodecamp.org/')
 wait = WebDriverWait(driver, 15)
 
-
-#svg = driver.find_elements_by_xpath('//*[@id="___gatsby"]/div/main/div/div[4]/ul/li[1]/div/svg')
-#svg.click()
-#svg1 = driver.find_elements_by_xpath('//*[@id="___gatsby"]/div/main/div/div[4]/ul/li[1]/ul/li[1]/div/svg')
-#svg1.click()
-# Cannot click svgs
-
-#try:
-#    li = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'superblock open')))
-#except TimeoutException:
-#    wait = wait
-
-#close_list = driver.find_element_by_xpath('//*[@class="superblock open"]/div')
-#close_list.click()
-#close_list1 = driver.find_element_by_xpath('//*[@class="block open"]/div')
-#close_list1.click()
 modules = driver.find_elements_by_class_name('superblock ')
-#li = driver.find_element_by_class_name('superblock open') InvalidSelectorException
-#li = driver.find_element_by_class_name('block open') Invalid SelectorExeption
 
 children = list()
 for module in modules:

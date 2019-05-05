41f7dd36a464bd724073c2321485a823bb89bee3
wtata <wtata@protonmail.com>
2019-05-05 13:15:03 -0400

    Locate children of div class map-ui

--- scraper.py
+++ scraper.py
@@ -26,39 +26,50 @@ file.close()
 read_file = open('first_child.txt', 'r', encoding='UTF-8')
 read_lines = read_file.readlines()
 #print("reading lines")
-for line in read_lines[:3]:
-    # Omit \n from dir name
-    l = line[:-1]
-    os.mkdir(l)
-    os.chdir(l)
-    #print("changing dirs")
-    if line == read_lines[0]:
-        for mod in read_lines[-7:]:
-            if mod[-1] == '\n':
-                mod_dir = mod[:-1]
-                os.mkdir(mod_dir)
-            else:
-                os.mkdir(mod)
-            #print("making a dir")
-for ex in read_lines[3:]:
-    if ex != "Not Passed\n":
-        # Omit 'Not Passed\n' and '\n'
-        if "Not Passed" in ex:
-            if ex[-1] == '\n':
-                ex = ex[10:-1]
-            else:
-                ex = ex[10:]
-            ex_file = open(ex + '.html', 'w', encoding='UTF-8')
+try:
+    for line in read_lines[:3]:
+        # Omit \n from dir name
+        l = line[:-1]
+        os.mkdir(l)
+        os.chdir(l)
+        #print("changing dirs")
+        if line == read_lines[0]:
+            for mod in read_lines[-7:]:
+                if mod[-1] == '\n':
+                    mod_dir = mod[:-1]
+                    os.mkdir(mod_dir)
+                else:
+                    os.mkdir(mod)
+                #print("making a dir")
+    for ex in read_lines[3:]:
+        if ex != "Not Passed\n":
+            # Omit 'Not Passed\n' and '\n'
+            if "Not Passed" in ex:
+                if ex[-1] == '\n':
+                    ex = ex[10:-1]
+                else:
+                    ex = ex[10:]
+                ex_file = open(ex + '.html', 'w', encoding='UTF-8')
+except FileExistsError:
+    pass
 
-wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'block ')))
+click = wait.until(EC.element_to_be_clickable((By.XPATH,
+                                               '//*[@id="___gatsby"]/div/main/div/div[4]/ul/li[1]/ul/li[2]')))
 print("waited long enough")
-elements = driver.execute_script('document.getElementsByClassName("block ")')
+click.click()
+print("clicked click")
+elements = driver.execute_script('document.getElementsByClassName("map-ui").children')
+print("found map-ui")
+#list_nodes = driver.execute_script('document.getElements.ByClassName("block")')
+#print("found closed lists")
 #elements.click()
 #print("clicked div")
 #element = driver.find_element_by_xpath('//*[@id="___gatsby"]/div/main/div/div[4]/ul/li[1]/ul/li[2]/div')
 #element.click()
+
 children = list()
-for element in elements:
-    children.append(element.text)
-    element.click()
-print(children)
+
+#for element in elements:
+    #children.append(element.text)
+    #element.click()
+print(elements)

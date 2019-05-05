7a353f7082daafa510305e8103defaf3267274e5
wtata <wtata@protonmail.com>
2019-05-05 09:52:12 -0400

    Need to fix chromedriver version error

--- scraper.py
+++ scraper.py
@@ -12,27 +12,53 @@ driver = webdriver.Chrome(chrome_options=options)
 driver.get('https://learn.freecodecamp.org/')
 wait = WebDriverWait(driver, 15)
 
-modules = driver.find_elements_by_class_name('superblock ')
+elements = driver.find_elements_by_class_name('superblock ')
 
 children = list()
-for module in modules:
+for element in elements:
     #print(module.text)
-    children.append(module.text)
+    children.append(element.text)
 #print(children)
 
-#file = open('first_child.txt', 'w', encoding='UTF-8')
-#file.writelines(children[0])
-
+file = open('first_child.txt', 'w', encoding='UTF-8')
+file.writelines(children[0])
+file.close()
 read_file = open('first_child.txt', 'r', encoding='UTF-8')
 read_lines = read_file.readlines()
+#print("reading lines")
 for line in read_lines[:3]:
     # Omit \n from dir name
-    line = line[:-2]
-    os.mkdir(line)
-    os.chdir(line)
+    l = line[:-1]
+    os.mkdir(l)
+    os.chdir(l)
+    #print("changing dirs")
+    if line == read_lines[0]:
+        for mod in read_lines[-7:]:
+            if mod[-1] == '\n':
+                mod_dir = mod[:-1]
+                os.mkdir(mod_dir)
+            else:
+                os.mkdir(mod)
+            #print("making a dir")
 for ex in read_lines[3:]:
     if ex != "Not Passed\n":
         # Omit 'Not Passed\n' and '\n'
-        ex = ex[11:-2]
-        #ex_file = open(ex + '.html', 'w', encoding='UTF-8')
-        print(ex)
+        if "Not Passed" in ex:
+            if ex[-1] == '\n':
+                ex = ex[10:-1]
+            else:
+                ex = ex[10:]
+            ex_file = open(ex + '.html', 'w', encoding='UTF-8')
+
+wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'block ')))
+print("waited long enough")
+elements = driver.execute_script('document.getElementsByClassName("block ")')
+#elements.click()
+#print("clicked div")
+#element = driver.find_element_by_xpath('//*[@id="___gatsby"]/div/main/div/div[4]/ul/li[1]/ul/li[2]/div')
+#element.click()
+children = list()
+for element in elements:
+    children.append(element.text)
+    element.click()
+print(children)

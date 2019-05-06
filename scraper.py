b77c5e9cc1253d74a66984ccbd6b3d72e62a8fa1
wtata <wtata@protonmail.com>
2019-05-05 20:03:20 -0400

    Refactor into curriculum, iterate functions

--- scraper.py
+++ scraper.py
@@ -13,55 +13,77 @@ wait = WebDriverWait(driver, 15)
 
 elements = driver.find_elements_by_class_name('superblock ')
 #elements = driver.find_elements_by_tag_name('li')
-children = list()
 
 for element in elements[-6:]:
     #children.append(element.text)
     element.click()
 
+children = list()
 file = open('first_child.txt', 'w', encoding='UTF-8')
+
 for element in elements:
-    #print(module.text)
     p = 'Projects'
     if p in element.text:
         n = p + '\n'
         new_text = str(element.text).replace(p, n)
         file.writelines(new_text)
+        children.append(new_text)
     else:
+        children.append(element.text)
         file.writelines(element.text)
 
 #print(children)
 file.close()
-read_file = open('first_child.txt', 'r', encoding='UTF-8')
-read_lines = read_file.readlines()
-#print("reading lines")
-try:
-    for line in read_lines[:3]:
-        # Omit \n from dir name
-        l = line[:-1]
-        os.mkdir(l)
-        os.chdir(l)
-        #print("changing dirs")
-        if line == read_lines[0]:
-            for mod in read_lines[-7:]:
-                if mod[-1] == '\n':
-                    mod_dir = mod[:-1]
-                    os.mkdir(mod_dir)
-                else:
-                    os.mkdir(mod)
-                #print("making a dir")
-    for ex in read_lines[3:]:
-        if ex != "Not Passed\n":
-            # Omit 'Not Passed\n' and '\n'
-            if "Not Passed" in ex:
-                if ex[-1] == '\n':
-                    ex = ex[10:-1]
-                else:
-                    ex = ex[10:]
-                ex_file = open(ex + '.html', 'w', encoding='UTF-8')
 
-except FileExistsError:
-    pass
+
+def iterate(line):
+    if '(300 hours)' or 'Interview' in line:
+        module_dir = line[:-1]
+        os.mkdir(module_dir)
+        os.chdir(module_dir)
+    elif "Not Passed" in line:
+        # Omit 'Not Passed\n' and '\n'
+        if line != "Not Passed\n":
+            if line[-1] == '\n':
+                line = line[10:-1]
+            else:
+                line = line[10:]
+            ex_file = open(line + '.html', 'w', encoding='UTF-8')
+            ex_file.close()
+    else:
+        os.mkdir(line[:-1])
+        os.chdir(line[:-1])
+
+
+def fcc_curriculum():
+
+    read_file = open('first_child.txt', 'r', encoding='UTF-8')
+    read_lines = read_file.readlines()
+    try:
+        for lines in read_lines[:3]:
+            # Omit \n from dir name
+            l = lines[:-1]
+            os.mkdir(l)
+            os.chdir(l)
+
+        for lines in read_lines[3:]:
+            for child in children[0]:
+                if lines in child:
+                        #child = child
+                        iterate(lines)
+        os.chdir('..')
+        os.chdir('..')
+        for lines in read_lines[3:]:
+            for child in children[1:]:
+                if lines in child:
+                    iterate(lines)
+                    if lines == child[-len(lines):] or lines == child[:len(lines)]:
+                        os.chdir('..')
+        file.close()
+
+    except FileExistsError:
+        pass
+
 
 clicks = list()
 click = wait.until(EC.element_to_be_clickable((By.XPATH,
@@ -75,4 +97,4 @@ elements = driver.find_elements_by_class_name('superblock ')
 
 print("found map-ui")
 
-children = list()
+
